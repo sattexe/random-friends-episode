c617d6ee90699eadabd9836fce3d2564fdc53300
Sattiyan Selvarajah <52833600+sattexe@users.noreply.github.com>
2022-12-30 17:37:59 +0800

    Initial Commit

---
+++ assets/js/script.js
@@ -0,0 +1,85 @@
+window.episodes = [];
+
+var headings = [
+    "Remember this one?",
+    "Oh, this one is great!",
+    "I love this one!"
+];
+
+var getEpisodes = function() {
+    $.getJSON('https://dev.sattiyans.com/random-friends-episode/require/data/friends.json', function(episodes) {
+            console.info('Episodes loaded!', episodes);
+            window.episodes = episodes;
+        })
+        .done(function() {
+            init();
+        })
+        .fail(function() {
+            $('.card').replaceWith('<button class="error" onClick="window.location.reload()">Error loading episodes, click here to reload.</button>');
+        });
+};
+
+var randomItem = function(array) {
+    return array[Math.floor(Math.random() * array.length)];
+};
+
+var getRandomEpisode = function() {
+    var season = randomItem(window.episodes),
+        episode = randomItem(season.episodes);
+
+    // add logic to make sure the first episode in 2-part episodes always comes first?
+
+    console.log('random episode: ', season.season, episode);
+
+    // Update the URLs
+    $('.card a')
+        .attr('href', 'https://www.netflix.com' + episode.urls.netflix)
+        .eq(0).text('S' + season.season + ' E' + episode.episode + ' - ' + episode.title);
+
+    // Update the header
+    $('h1').text(randomItem(headings));
+
+    // Update the elements for data tracking
+    $('a, button', '.card').data({
+        episode: episode.episode + ' - ' + episode.title,
+        season: season.season
+    });
+
+    // TODO: Add episode descriptions to JSON and load them into card
+};
+
+// event tracking
+var trackCardClick = function($target) {
+    var action = $target.data('gaAction');
+    var label = $target.data('episode');
+    var value = $target.data('season');
+    var event = {
+        'event_category': 'Card Click',
+        'event_label': label,
+        'value': value
+    };
+
+    window.gtag('event', action, event);
+};
+
+// I don't love this name. What's better, or is there a better way to do this altogether?
+var init = function() {
+    getRandomEpisode();
+
+    // Show the card if hidden
+    $('.card.hidden').removeClass('hidden');
+
+    // Bind UI handlers
+    $('a, button', '.card').on('click', function() {
+        trackCardClick($(this));
+    });
+
+    $('.card button').on('click', function() {
+        getRandomEpisode();
+    });
+};
+
+// All set, let's go!
+$(function() {
+    getEpisodes();
+});
---
+++ index.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<title>The One with Random F.R.I.E.N.D.S Episode</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="sattiyans. is a professional web developer based in Malaysia. Feel free to email at hello@sattiyans.com.">
+    <meta name="keywords" content="sattiyans, webdeveloper, web, developer, php, shopify, woocommerce, malaysia developer, malaysia web developer, backend developer, sattiyan, sattiyan selvarajah" />
+    <meta name="author" content="sattiyans.">
+
+    <meta name=”robots” content="index, follow">
+    <meta http-equiv="content-language" content="en-us">
+
+    <meta property="og:title" content="The One with Random F.R.I.E.N.D.S Episode" />
+    <meta property="og:description" content="sattiyans. is a professional web developer based in Malaysia. Feel free to email at hello@sattiyans.com." />
+    <meta property="og:image" content="https://www.sattiyans.com/images/sattiyans.png" />
+
+    <meta name="twitter:title" content="The One with Random F.R.I.E.N.D.S Episode" />
+    <meta name="twitter:image" content="https://www.sattiyans.com/images/sattiyans.png" />
+    <meta name="twitter:creator" content="@sattiyans" />
+    
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="icon" href="https://i.ibb.co/FVM0B7Z/me.jpg">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500;600&display=swap" rel="stylesheet">
+    <script src="https://unpkg.com/feather-icons"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer">
+
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+    <script src="assets/js/script.js" defer></script>
+   
+    <style>
+
+
+/* CTA NETFLIX */
+
+.netflix {
+    height: 30px;
+    /* width: 68px; */
+    background-color: white;
+    padding: 6px 30px 6px 30px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+    z-index: 1;
+    box-shadow: inset 0 0 20px rgba(255, 166, 170, 0);
+    outline: 1px solid;
+    outline-color: rgba(255, 166, 170, .5);
+    outline-offset: 0px text-shadow: none;
+    transition: all 1250ms cubic-bezier(0.19, 1, 0.22, 1);
+}
+
+.netflix-a {
+  font-family: 'Roboto', sans-serif;
+  font-weight: bold;
+  width: inherit;
+  position: absolute;
+  text-decoration: none;
+  color: #E50914;
+  z-index: 1;
+  transition: color .2s;
+  z-index: 3;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%,-50%);
+}
+
+.netflix-a .is-white {
+  color: white;
+}
+
+
+.background__button {
+  position: absolute;
+  width: 0;
+  height: 0;
+  background-color: #E50914;
+  border-radius: 100px;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%,-50%);
+  z-index: 2;
+  transition: width .3s, height .3s, box-shadow .3s;
+}
+
+
+.background__button.is-hover {
+   width: 200px;
+  height: 200px;
+  box-shadow: inset 0px 0px 300px -40px     rgba(0,0,0,1);
+}
+
+.netflix:hover {
+  box-shadow: inset 0 0 20px rgba(255, 166, 170, .5), 0 0 20px rgba(255, 126, 133, .8);
+  outline-color: rgba(255, 166, 170, 0);
+  outline-offset: 15px;
+  text-shadow: 1px 1px 2px #427388; 
+}
+
+    </style>
+</head>
+
+<body>
+    <main>
+    
+    <div class="container" style="text-align: center; padding: 50% 0;">
+        <h2 style="text-align: center; margin-bottom: 10px;">The One with Random<br>
+            <img width="150px" style="margin-top: 10px;" src="assets/img/friends-logo.png" alt=""><br>
+            Episode
+        </h2>
+        <article class="card hidden">
+            <header>
+                <h3><a href="#" target="_blank" data-ga-action="watch">..</a></h3>
+            </header>
+            <footer>
+            <div class="netflix" style="margin-top: 10px;">
+                <a class="netflix-a" target="_blank" href="">Watch on Netflix</a>
+                <div class="background__button"></div>
+            </div>
+            <div style="margin-top: 20px;">
+                <button style="color: #fff; background-color: #000; cursor:pointer;" onClick="window.location.reload();"><i style="text-align: center; color: #fff; width: 80px;" data-feather="refresh-cw"></i></button>
+            </div>
+            </footer>
+        </article>
+
+    </div>
+        <!-- <div style="text-align: center; margin-bottom: 80px; position: fixed; bottom: 0px; left: 0px; right: 0px;">
+            <a style="font-size: smaller; color: #fff; text-align: center;" href="https://github.com/sattexe/" target="_blank" rel="noopener">
+                Source Code
+            </a>
+        </div> -->
+        <!-- <div style="text-align: center; margin-bottom: 25px; position: fixed; bottom: 0px; left: 0px; right: 0px;">
+            <a style="color: #fff; text-align: center;" href="https://github.com/sattexe/" target="_blank" rel="noopener" title="Click to visit my Github profile">
+                <i style="text-align: center; color: #fff; margin-right: 15px;" data-feather="github"></i>
+            </a>
+            <a style="color: #fff;" href="https://linkedin.com/in/sattiyan/" target="_blank" rel="noopener" title="Click to visit my Linkedin profile">
+                <i style="text-align: center; color: #fff; margin-right: 15px;" data-feather="linkedin"></i>
+            </a>
+            <a style="color: #fff; text-align: center;" href="https://tvtime.com/r/22DAT" target="_blank" rel="noopener" title="Click to visit my TV Time profile">
+                <i style="text-align: center; color: #fff; margin-right: 15px;" data-feather="tv"></i>
+            </a>
+            <a style="color: #fff;" href="https://boxd.it/4VvZ1" target="_blank" rel="noopener" title="Click to visit my Letterboxd profile">
+                <i style="text-align: center; color: #fff; margin-right: 15px;" data-feather="film"></i>
+            </a>
+            <a style="color: #fff;" href="https://instagram.com/ssattiyan/" target="_blank" rel="noopener" title="Click to visit my Instagram profile">
+                <i style="text-align: center; color: #fff; margin-right: 15px;" data-feather="instagram"></i>
+            </a>
+            <a style="color: #fff;" href="https://twitter.com/sattiyans/" target="_blank" rel="noopener" title="Click to visit my Twitter profile">
+                <i style="text-align: center; color: #fff; margin-right: 15px;" data-feather="twitter"></i>
+            </a>
+        </div> -->
+    </main>
+    <script>
+        feather.replace()
+    </script>
+</body>
+
+</html>
